3b4703aa91e52246a93d7ed7fef8609499928388
leeadh <leexhadrian@gmail.com>
2018-09-05 21:24:14 +0800

    Update sampleApp.js

--- sampleApp.js
+++ sampleApp.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello hehehe!'))
+app.get('/', (req, res) => res.send('Hello try!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
